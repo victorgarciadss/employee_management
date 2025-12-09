8a5c9a53cff82822f9aac267e2d08a91b94fb6e9
victorgarciadss <garciavictor.1308@gmail.com>
2025-12-09 10:23:54 -0300

    chore: initial configs in frontend

---
+++ employeePortal/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={} /> */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
+
+
