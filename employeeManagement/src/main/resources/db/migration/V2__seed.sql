e64aa2b20347b9d622640bb5ea1337b9e7d6e715
victorgarciadss <garciavictor.1308@gmail.com>
2025-12-09 01:22:24 -0300

    feat: add seed migration

---
+++ employeeManagement/src/main/resources/db/migration/V2__seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_users (name, email, password)
+VALUES ('Pedro Souza', 'pedro@gmail.com', '$2a$10$Elox7H4QW.FqVNNeyeBcUeQONKoLWOd9bm3YEAS1IevdpjBSq1gyu');
+
+INSERT INTO tb_employees (name, admission_date, salary, status)
+VALUES ('Felipe Pereira', '2025-03-28', 3000.00, 'Ativo');
+
+
